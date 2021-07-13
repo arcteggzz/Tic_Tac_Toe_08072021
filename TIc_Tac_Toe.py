829fc22b1045ae36f81f0b290457e9d0b9d38019
arcteggzz <arcteggzz@gmail.com>
2021-07-13 23:34:46 +0100

    Added Comments
    I attempted to add relevant comments to the code. Stopped before I was done tho.

--- TIc_Tac_Toe.py
+++ TIc_Tac_Toe.py
@@ -1,98 +1,164 @@
-"""
-"""
-
 from typing import SupportsIndex
 
-
+#Remaining tasks
+#2. Make each players move to not be the same. I haven't achieved this yet.
+"""
+I tried creating a new function for both player 1 and 2 respectively.
+The function is called when the player makes his second move of the game.
+The function checks if the move entered co-incides with a number (1-9)
+Then it checks if that square is occupied already.
+But for some reasn the function desn't work properly.
+Still very buggy
+"""
+#3. Print the 1-9 for the board.
+#4. Make computer make moves.
 def main():
+    #This initializes the beggining of the game.
     one = " - "
     two = " - "
     three = " - "
     four = " - "
     five = " - "
-    six =" - "
+    six = " - "
     seven = " - "
     eight = " - "
     nine = " - "
+    #Call the print function that prints out the empty board.
     print_board(one, two, three, four, five, six, seven, eight, nine)
-    turn = 0
-    turns_one = 0
-    turns_two = 0
-    while True:
+    turn = 0 #Variable that helps swith the players turns from player 1 back to player 2 continously.
+    turns_one = 0 #tracks how many times player 1 has played
+    turns_two = 0 #tracks how many times player 2 has played
+    #Call the check winner function
+    """
+    This checks if there is a winner of the game.
+    It takes in the 1-9 variables and checks if they are filled with the correct data to determine a winner.
+    It then returns a variable(over).
+    Over is a boolean variable.
+    """
+    over = check_if_winner_1(one, two, three, four, five, six, seven, eight, nine)
+    while over:
+        print (over)#I use this to check the state of Over  (true or false)
+        # The If statement below is the draw checker.
+        #If all squares are filled, game is a draw and you immediately break out of the while loop.
+        if one != " - " and two != " - " and three != " - " and four != " - " and five != " - " and six != " - " and seven != " - " and eight != " - " and nine != " - ":
+            print ('Game is a draw')
+            break
         if turn == 0:
             turn = turn + 1
             turns_one = turns_one + 1
-            player_one_move = get_player_one_move(turns_one)
+            if turns_one == 1:
+                #run a special checker for move 1
+                player_one_move = get_first_player_one_move(turns_one)
+            else:
+                #run the other checker for moves 2 and above
+                player_one_move = get_player_one_move(turns_one, one, two, three, four, five, six, seven, eight, nine)
             one, two, three, four, five, six, seven, eight, nine = update_board_one(one, two, three, four, five, six, seven, eight, nine, player_one_move)
             print_board(one, two, three, four, five, six, seven, eight, nine)
-            if turns_one > 2:
-                check_if_winner_1(one, two, three, four, five, six, seven, eight, nine)
+            over = check_if_winner_1(one, two, three, four, five, six, seven, eight, nine)
                 
-
         else:
             turn = turn - 1
             turns_two = turns_two + 1
-            player_two_move = get_player_two_move(turns_two)
+            if turns_two == 1:
+                #run a special checker for move 1
+                player_two_move = get_first_player_two_move(turns_two)
+            else:
+                #run the other checker for moves 2 and above
+                player_two_move = get_player_two_move(turns_two, one, two, three, four, five, six, seven, eight, nine)
             one, two, three, four, five, six, seven, eight, nine = update_board_two(one, two, three, four, five, six, seven, eight, nine, player_two_move)
             print_board(one, two, three, four, five, six, seven, eight, nine)
-            if turns_two > 2:
-                check_if_winner_2(one, two, three, four, five, six, seven, eight, nine)
-                break
-    print ('The game is Over')
-
+            over = check_if_winner_2(one, two, three, four, five, six, seven, eight, nine)
+    print ('The game is Over') 
+        
 def check_if_winner_1(one, two, three, four, five, six, seven, eight, nine):
+    over = True
     #1
     if one == ' X ' and two == ' X ' and three == ' X ':
         print ("Player 1 is the winner")
-        break
+        over = False
+        return over
     #2
     if one == ' X ' and five == ' X ' and nine == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #3
     if one == ' X ' and four == ' X ' and seven == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #4
     if two == ' X ' and five == ' X ' and eight == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #5
     if three == ' X ' and six == ' X ' and nine == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #6
     if three == ' X ' and five == ' X ' and seven == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #7
     if four == ' X ' and five == ' X ' and six == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
     #8
     if seven == ' X ' and eight == ' X ' and nine == ' X ':
         print ("Player 1 is the winner")
+        over = False
+        return over
+    else:
+        return over
 
 def check_if_winner_2(one, two, three, four, five, six, seven, eight, nine):
+    over = True
     #1
     if one == ' O ' and two == ' O ' and three == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #2
     if one == ' O ' and five == ' O ' and nine == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #3
     if one == ' O ' and four == ' O ' and seven == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #4
     if two == ' O ' and five == ' O ' and eight == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #5
     if three == ' O ' and six == ' O ' and nine == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #6
     if three == ' O ' and five == ' O ' and seven == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #7
     if four == ' O ' and five == ' O ' and six == ' O ':
         print ("Player 2 is the winner")
+        over = False
+        return over
     #8
     if seven == ' O ' and eight == ' O ' and nine == ' O ':
         print ("Player 2 is the winner")
-
+        over = False
+        return over
+    else:
+        return over
 
 def update_board_two(one, two, three, four, five, six, seven, eight, nine, player_two_move):
     if player_two_move == 1:
@@ -152,7 +218,11 @@ def update_board_one(one, two, three, four, five, six, seven, eight, nine, playe
         nine = ' X '
         return one, two, three, four, five, six, seven, eight, nine
 
-def get_player_one_move(turns_one):
+#Get Players Moves
+"""
+Get Player 1 move for turn 1.
+"""
+def get_first_player_one_move(turns_one):
     while True:
         turns_one = str(turns_one)
         player_one_move = int(input("Player one, Enter your " + (turns_one) + " move: "))
@@ -160,22 +230,73 @@ def get_player_one_move(turns_one):
             return player_one_move
         print ("Enter a valid move")
 
+"""
+Get Player 1 move for turn 2, 3, 4, and 5.
+"""
+def get_player_one_move(turns_one, one, two, three, four, five, six, seven, eight, nine):
+    while True:
+        turns_one = str(turns_one)
+        player_one_move = int(input("Player one, Enter your " + (turns_one) + " move: "))
+        if is_valid_move_1(player_one_move) and non_duplicate_checker_1(player_one_move, one, two, three, four, five, six, seven, eight, nine):
+            return player_one_move
+        print ("Enter a valid move")
+
+"""
+Non Duplicate Checker for Player 1 for turn 2, 3, 4, and 5.
+"""
+def non_duplicate_checker_1(player_one_move, one, two, three, four, five, six, seven, eight, nine):
+    if player_one_move == 1 and (one != " X " or one != " O "):
+        return player_one_move
+    if player_one_move == 2 and (two != " X " or two != " O "):
+        return player_one_move
+"""
+Number clicked Checker for Player 1
+"""
 def is_valid_move_1(player_one_move):
     if 0 < player_one_move < 10:
         return player_one_move
 
-def get_player_two_move(turns_two):
+"""
+Get Player 2 move for turn 1.
+"""
+def get_first_player_two_move(turns_two):
     while True:
         turns_two = str(turns_two)
         player_two_move = int(input("Player two, Enter your " + (turns_two) + " move: "))
-        if is_valid_move_2(player_two_move):
+        if is_valid_move_1(player_two_move):
             return player_two_move
         print ("Enter a valid move")
 
+"""
+Get Player 2 move for turn 2, 3, 4, and 5.
+"""
+def get_player_two_move(turns_two, one, two, three, four, five, six, seven, eight, nine):
+    while True:
+        turns_two = str(turns_two)
+        player_two_move = int(input("Player two, Enter your " + (turns_two) + " move: "))
+        if is_valid_move_2(player_two_move) and non_duplicate_checker_2(player_two_move, one, two, three, four, five, six, seven, eight, nine):
+            return player_two_move
+        print ("Enter a valid move")
+
+"""
+Non Duplicate Checker for Player 2
+"""
+def non_duplicate_checker_2(player_two_move, one, two, three, four, five, six, seven, eight, nine):
+    if player_two_move == 1 and (one != " X " or one != " O "):
+        return player_two_move
+
+"""
+Number clicked Checker for Player 2
+"""
 def is_valid_move_2(player_two_move):
     if 0 < player_two_move < 10:
         return player_two_move     
 
+
+
+"""
+Print the current game state(Board)
+"""
 def print_board(one, two, three, four, five, six, seven, eight, nine):
     print (one + two + three)
     print (four + five + six)
